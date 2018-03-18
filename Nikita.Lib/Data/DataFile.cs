271a0179b20d54eb8da2e91c8c8b483358736a1b
nikita <nikita.nosov@gmail.com>
2018-03-18 00:30:11 -0500

    update to unit tests

--- Nikita.Lib/Data/DataFile.cs
+++ Nikita.Lib/Data/DataFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nikita.Lib.Interfaces;
 
 namespace Nikita.Lib.Data
 {
--- Nikita.Lib/Interfaces/IDataFile.cs
+++ Nikita.Lib/Interfaces/IDataFile.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Nikita.Lib.Data;
 
-namespace Nikita.Lib.Data
+namespace Nikita.Lib.Interfaces
 {
     public interface IDataFile<T>  
     {
